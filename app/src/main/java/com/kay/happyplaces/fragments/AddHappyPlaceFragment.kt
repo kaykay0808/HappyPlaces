8f844a7bfb18f4088b1646bba8a8d994488c6471
kay <kayshiro@live.no>
2022-03-12 19:35:23 +0100

    second fragment created

--- app/src/main/java/com/kay/happyplaces/fragments/AddHappyPlaceFragment.kt
+++ app/src/main/java/com/kay/happyplaces/fragments/AddHappyPlaceFragment.kt
@@ -1,24 +1,26 @@
 package com.kay.happyplaces.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.kay.happyplaces.R
+import com.kay.happyplaces.databinding.FragmentAddHappyPlaceBinding
 import com.kay.happyplaces.databinding.FragmentHappyPlacesBinding
 
-class HappyPlacesFragment : Fragment() {
 
-    private var _binding: FragmentHappyPlacesBinding? = null
+class AddHappyPlaceFragment : Fragment() {
+
+    private var _binding: FragmentAddHappyPlaceBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentHappyPlacesBinding.inflate(inflater, container, false)
+        _binding = FragmentAddHappyPlaceBinding.inflate(inflater, container, false)
 
         return binding.root
     }
