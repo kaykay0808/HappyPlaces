5735a9ee4770164e47b15ddde375e65b46eb5093
kay <kayshiro@live.no>
2022-03-13 23:51:15 +0100

    setting up the add layout

--- app/src/main/java/com/kay/happyplaces/fragments/AddHappyPlaceFragment.kt
+++ app/src/main/java/com/kay/happyplaces/fragments/AddHappyPlaceFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.kay.happyplaces.databinding.FragmentAddHappyPlaceBinding
 
 class AddHappyPlaceFragment : Fragment() {
@@ -20,6 +22,22 @@ class AddHappyPlaceFragment : Fragment() {
         _binding = FragmentAddHappyPlaceBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Hide actionBar on this fragment
+        // (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        // toolbar setup for fragments
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarAddPlace)
+        // Back button setup on toolbar for fragments
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // navigate back with navHost
+        binding.toolbarAddPlace.setNavigationOnClickListener {
+            findNavController().popBackStack()
+            // customDialogForBackButton()
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/kay/happyplaces/fragments/ListHappyPlacesFragment.kt
+++ app/src/main/java/com/kay/happyplaces/fragments/ListHappyPlacesFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kay.happyplaces.R
 import com.kay.happyplaces.databinding.FragmentListHappyPlacesBinding
 
 class ListHappyPlacesFragment : Fragment() {
@@ -21,6 +23,17 @@ class ListHappyPlacesFragment : Fragment() {
         _binding = FragmentListHappyPlacesBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        // Hide actionBar on this fragment
+        // (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        // Navigation with floating button to add fragment
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_happyPlacesFragment_to_addHappyPlaceFragment)
+        }
     }
 
     override fun onDestroy() {
